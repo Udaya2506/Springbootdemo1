7810a5132966968e211336ae3ec59868206381b5
Udaya2506 <sonylaxmi2506@gmail.com>
2024-12-16 15:48:54 -0800

    added code

--- LSpringBootECSPL1/src/main/java/com/example/LSpringBootECSPL1/LSpringBootEcspl1Application.java
+++ LSpringBootECSPL1/src/main/java/com/example/LSpringBootECSPL1/LSpringBootEcspl1Application.java
@@ -2,6 +2,8 @@ package com.example.LSpringBootECSPL1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LSpringBootEcspl1Application {
@@ -9,5 +11,12 @@ public class LSpringBootEcspl1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(LSpringBootEcspl1Application.class, args);
 	}
+	 @RestController
+	    public static class HelloController {
+	        @GetMapping("/")
+	        public String hello() {
+	            return "Hello, Spring Boot!";
+	        }
+	    }
 
 }
